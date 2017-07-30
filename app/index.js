2435d00bc0697ec7caf231351fe069bf60c905e3
mshockwave <yihshyng223@gmail.com>
2017-07-30 11:25:15 -0400

    Simplify Default Entry Module

--- app/index.js
+++ app/index.js
@@ -1,25 +1,13 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-  prompting(){
-    this.prompt({
-      name: 'subGenerator',
-      message: 'Choose the project type you want to generate',
-      type: 'list',
-      choices: [
-        'Pass',
-        'Executable'
-      ],
-      default: 'Pass'
-    }).then((answer) => {
-      switch(answer.subGenerator){
-        case 'Pass':
-          this.composeWith(require.resolve('../pass'));
-          break;
-        case 'Executable':
-          this.composeWith(require.resolve('../executable'));
-          break;
-      }
-    });
+  default(){
+    this.log(`
+    Please use the sub-module instead:
+      
+      yo llvm:executable <arguments>
+
+      yo llvm:pass <arguments>
+    `);
   }
 }
